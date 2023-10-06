8822c9b2f9551712de04046da32b1ea2ba507fde
nolanzhong <nolan.zhong@duke.edu>
2023-10-05 22:01:53 -0400

    added stock percentages with API

--- mainScreenScript.js
+++ mainScreenScript.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             newStock.textContent = newStockText;
             stockList.appendChild(newStock);
             stocks.push(newStockText);
-            createStockCircles();
+            getStockPercentages(stocks);
             stockInput.value = ""; //Clear input field
         }
     });
@@ -39,38 +39,68 @@ document.addEventListener("DOMContentLoaded", function() {
             addStockButton.click(); //Trigger add stock on clicking enter
         }
     });
+
+    
+    console.log(stocks);
+    getStockPercentages(stocks); // Initial creation of circles
+    
 });
 
 // script.js
 const stockContainer = document.getElementById('stock-container');
 const stocks = ['AAPL', 'GOOGL', 'AMZN', 'TSLA', 'MSFT']; // Your list of stocks
-
-function createStockCircles() {
-    stockContainer.innerHTML = ''; // Clear existing circles
-
-    stocks.forEach(stock => {
-        const circle = document.createElement('div');
-        circle.classList.add('stock-circle');
-        circle.textContent = stock;
-        stockContainer.appendChild(circle);
-    });
-}
+const stockPercentages = []
 
 function createStockCircles() {
     stockContainer.innerHTML = ''; // Clear existing circles
 
     const circleSize = 500 * (0.95**stocks.length); // Calculate circle size based on the number of stocks
 
-    stocks.forEach(stock => {
+    for (let i = 0; i < stocks.length; i++) {
+        const stock = stocks[i];
+        const percentage = stockPercentages[i]; // Get the corresponding percentage
+    
         const circle = document.createElement('div');
         circle.classList.add('stock-circle');
-        circle.textContent = stock;
+        circle.textContent = `${stock} (${percentage}%)`; // Include the percentage in the text content
         circle.style.width = `${circleSize}px`; // Apply dynamic size
         circle.style.height = `${circleSize}px`;
         stockContainer.appendChild(circle);
-    });
+      }
 }
 
-createStockCircles(); // Initial creation of circles
+// Replace 'YOUR_API_KEY' with your actual Twelve Data API key
+const apiKey = 'ckflhl1r01qovk256600ckflhl1r01qovk25660g';
 
-// You can add more stocks to the array and call createStockCircles() again to update the display.
+function fetchStockData(symbol) {
+    const apiUrl = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`;
+  
+    return fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const stockPriceChange = (((data.c - data.pc) / data.pc) * 100).toFixed(2);
+        return stockPriceChange;
+      })
+      .catch(error => {
+        console.error(`Fetch error for ${symbol}:`, error);
+        return null;
+      });
+  }
+  
+  function getStockPercentages(lstStocks) {
+    const promises = lstStocks.map(stock => fetchStockData(stock));
+  
+    Promise.all(promises)
+      .then(results => {
+        stockPercentages.push(...results); // Push the results into stockPercentages
+        createStockCircles();
+      })
+      .catch(error => {
+        console.error('Error fetching stock data:', error);
+      });
+  }
